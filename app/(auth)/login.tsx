9a447e860033191a18023904aeb8f5199eeb2700
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-04-03 21:53:51 +0530

    chore: login screen

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,57 @@
+import { Button, ButtonText } from "@/components/ui/button";
+import { FormControl } from "@/components/ui/form-control";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { EyeIcon, EyeOffIcon } from "@/components/ui/icon";
+import React from "react";
+import { HStack } from "@/components/ui/hstack";
+
+export default function LoginScreen() {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleState = () => {
+        setShowPassword((showState) => {
+            return !showState;
+        });
+    };
+    return (
+        <FormControl className="p-4 border rounded-lg border-outline-300">
+            <VStack space="xl">
+                <Heading className="text-typography-900">Login</Heading>
+                <VStack space="xs">
+                    <Text className="text-typography-500">Email</Text>
+                    <Input className="min-w-[250px]">
+                        <InputField type="text" />
+                    </Input>
+                </VStack>
+                <VStack space="xs">
+                    <Text className="text-typography-500">Password</Text>
+                    <Input className="text-center">
+                        <InputField type={showPassword ? "text" : "password"} />
+                        <InputSlot className="pr-3" onPress={handleState}>
+                            <InputIcon
+                                as={showPassword ? EyeIcon : EyeOffIcon}
+                            />
+                        </InputSlot>
+                    </Input>
+                </VStack>
+                <HStack className="gap-2" space="sm">
+                    <Button
+                        className="flex-1"
+                        variant="outline"
+                        onPress={() => { }}
+                    >
+                        <ButtonText className="text-typography-0">Sign up</ButtonText>
+                    </Button>
+                    <Button
+                        className="flex-1"
+                        onPress={() => { }}
+                    >
+                        <ButtonText className="text-typography-0">Sign in</ButtonText>
+                    </Button>
+                </HStack>
+            </VStack>
+        </FormControl>
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,26 +5,35 @@ import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Icon } from '@/components/ui/icon';
-import { ShoppingCart } from 'lucide-react-native';
+import { ShoppingCart, UserRound } from 'lucide-react-native';
 import { useCart } from '@/store/cartStore';
 
 const queryClient = new QueryClient()
 
 export default function RootLayout() {
-    const item = useCart((state:any) => state.items);
+    const item = useCart((state: any) => state.items);
     return (
         <QueryClientProvider client={queryClient}>
             <GluestackUIProvider mode="light">
                 <Stack
                     screenOptions={{
                         headerRight: () => (
-                            <View style={{ padding: 14 }}>
-                                <Link href={"/cart"} asChild>
-                                    <Pressable className='flex-row items-center gap-2'>
-                                        <Icon as={ShoppingCart} />
-                                        <Text>{item.length}</Text>
-                                    </Pressable>
-                                </Link>
+                            <View className='flex-row'>
+                                <View className=' bg-gray-100 rounded-sm p-3'>
+                                    <Link href={"/cart"} asChild>
+                                        <Pressable className='flex-row items-center gap-2'>
+                                            <Icon as={ShoppingCart} />
+                                            <Text>{item.length}</Text>
+                                        </Pressable>
+                                    </Link>
+                                </View>
+                                <View className=' bg-gray-100 rounded-sm p-3'>
+                                    <Link href={"/login"} asChild>
+                                        <Pressable className='flex-row items-center gap-2'>
+                                            <Icon as={UserRound} />
+                                        </Pressable>
+                                    </Link>
+                                </View>
                             </View>
                         ),
                     }}
